bcbb3fc4acac504420946b3792e8cdb1f3446aa2
nhjnnh <K2420219B@e.ntu.edu.sg>
2024-05-18 14:17:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
 import google.generativeai as palm
-
-app = Flask(__name__)
+import replicate
+import os
 
 flag = 1
 name = ""
+
 palm.configure(api_key="AIzaSyCOlwyukHbptkuc8DUGINSt7tcZocNR9MU")
+os.environ["REPLICATE_API_TOKEN"] = r8_GDMp3MoafgnYn8CUBQ3Dj1rk8He5Zdu4HNjst
 
 model = {"model" : "models/chat-bison-001"}
+app = Flask(__name__)
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
@@ -38,6 +42,18 @@ def text_result_makersuite():
     q = request.form.get("q")
     r = palm.chat(**model, messages=q)
     return render_template("text_result_makersuite.html", r=r.last)
+
+@app.route("/generate_image", methods=["GET", "POST"])
+def generate_image():
+    return render_template("generate_image.html")
+
+@app.route("/image_result", methods=["GET", "POST"])
+def image_result():
+    q = request.form.get("q")
+    r = replicate.run("stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+      input = {"prompt":q}
+      )
+    return render_template("image_result.html", r=r[0])
 
 @app.route("/end", methods=["GET", "POST"])
 def end():
