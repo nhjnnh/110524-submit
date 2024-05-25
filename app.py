773bd7b2eb433d84a3dac0abf843a0d84399df36
nhjnnh <K2420219B@e.ntu.edu.sg>
2024-05-25 14:39:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 import google.generativeai as palm
 import replicate
 import os
+import sqlite3
+import datetime
+from flask import Markup
 
 flag = 1
 name = ""
@@ -21,6 +24,13 @@ def main():
     global flag, name
     if flag == 1:
         name = request.form.get("q")
+        current_time = datetime.datetime.now()
+        conn = sqlite3.connect('log.db')
+        c = conn.cursor()
+        c.execute("insert into user (name,time) values (?,?)",(name, current_time))
+        conn.commit()
+        c.close()
+        conn.close()
         flag = 0
     return render_template("main.html", r=name)
 
@@ -54,6 +64,30 @@ def image_result():
       input = {"prompt":q}
       )
     return render_template("image_result.html", r=r[0])
+
+@app.route("/log", methods=["GET", "POST"])
+def log():
+    conn = sqlite3.connect('log.db')
+    c = conn.cursor()
+    c.execute("select * from user")
+    r = ""
+    for row in c:
+        r += str(row) + "<br>"
+    print(r)
+    r = Markup(r)
+    c.close()
+    conn.close()
+    return render_template("log.html",r=r)
+
+@app.route("/delete", methods=["GET", "POST"])
+def delete():
+    conn = sqlite3.connect('log.db')
+    c = conn.cursor()
+    c.execute("Delete from user")
+    conn.commit(
+    c.close()
+    conn.close()
+    return render_template("delete.html")
 
 @app.route("/end", methods=["GET", "POST"])
 def end():
